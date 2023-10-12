d998a3b52293707059d6f3a0617b5dbb0e0699c2
Kola Kolade <kolakolade@gmail.com>
2023-10-12 20:42:59 +0100

    Add logo to hero section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './assets/css/style.css';
+import heroLogo from './assets/images/kolakolade_logo.png';
 import './assets/scss/main.scss';
+
+// Site logo
+const logoDiv = document.querySelector('.hero__logo');
+
+// Site logo
+logoDiv.src = heroLogo;
