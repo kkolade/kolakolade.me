ca28e2f2b43e2cc0e259ce40574f14e39ec61a32
Kola Kolade <kolakolade@gmail.com>
2023-10-16 15:54:36 +0100

    Clear index.html & index.js to start over

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './assets/css/style.css';
-import heroLogo from './assets/images/kolakolade_logo.png';
-import './assets/scss/main.scss';
-
-// Site logo
-const logoDiv = document.querySelector('.hero__logo');
-
-// Site logo
-logoDiv.src = heroLogo;
