11f27d02089eb0397fd9f62a037d45a46251116e
Kola Kolade <kolakolade@gmail.com>
2023-10-17 15:21:55 +0100

    Add html structureand content for about section

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Typewriter from 'typewriter-effect/dist/core';
 import './assets/css/style.css';
 import navbarLogo from './assets/images/kolakolade_logo.png';
 import './assets/scss/main.scss';
-
 // Import fontawesome
 import '@fortawesome/fontawesome-free/js/brands';
 import '@fortawesome/fontawesome-free/js/fontawesome';
